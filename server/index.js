5c94536d46e7d254692447cd1376e9a934b1b62d
itsvishu0070 <panditvishu003@gmail.com>
2025-09-10 16:33:14 +0530

    Update CORS configuration to use production URL

--- server/index.js
+++ server/index.js
@@ -11,8 +11,10 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", 
-    methods: ["GET", "POST","PUT"],
+    // origin: "http://localhost:5173",
+    origin: "https://proctorpro-2.onrender.com",
+
+    methods: ["GET", "POST", "PUT"],
   },
 });
 
